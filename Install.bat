ffc69a3c6e7e82c0d11670bd970bc568468df64d
HGreender <HGrender@users.noreply.github.com>
2025-04-02 23:27:12 +0300

    virtual env creation has been added

--- Install.bat
+++ Install.bat
@@ -1,5 +1,8 @@
 curl -o python-installer.exe https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
 python --version
-pip install telethon pandas asyncio
-pip install platform
+python3 -m venv .env
+.env\Scripts\activate
+pip install uv
+uv pip install telethon pandas asyncio --system
+uv pip install platform --system
