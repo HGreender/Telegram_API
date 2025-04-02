0ddfb00f3fb26d7f6070e009086c3e2279ffeef2
HGreender <HGrender@users.noreply.github.com>
2025-04-02 19:06:19 +0300

    Fix install file

--- Install.bat
+++ Install.bat
@@ -1,3 +1,5 @@
 curl -o python-installer.exe https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
 python --version
+pip install telethon pandas asyncio
+pip install platform
